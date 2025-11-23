714625ca3752fd4f3f194dd9848b4f9025bb95cc
zxraye <zxraye@gmail.com>
2025-11-24 00:37:30 +0800

    Day 1: Data types problems and solutions

---
+++ Sample Problems/day_one_solution.py
@@ -0,0 +1,19 @@
+# problem 1
+user_age = 21
+user_city = "Paris"
+is_learning_python = True
+print("My age is", user_age, "and I'm learning python from", user_city)
+
+# problem 2
+mystery_data = ["red", 42, 3.14]
+print(type(mystery_data))
+mystery_data[1] = "blue"
+print(mystery_data)
+another_mystery = "Immutability"
+print(type(another_mystery))
+
+# problem 3
+electrical_impedance = 10 + 5j
+print("Impedance Value:", electrical_impedance, "| Type:", type(electrical_impedance))
+student_info = {"id": 10069, "major": "Computer Science", "gpa": 1.25}
+print(student_info)
