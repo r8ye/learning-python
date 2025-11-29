9741923e3be936d635866bfa28f4baf972aaf242
zxraye <zxraye@gmail.com>
2025-11-29 22:38:07 +0800

    augmented assignments +=

---
+++ Learning Python/augmented_assignment.py
@@ -0,0 +1,101 @@
+# addition
+my_var = -2
+my_var += 5
+print(my_var)
+
+my_var_2 = 10
+my_var_2 += 126
+print(my_var_2)
+
+
+# subtraction
+my_var_3 = 1
+my_var_3 -= 10
+print(my_var_3)
+
+my_var_4 = 0
+my_var_4 -= 12.5
+print(my_var_4)
+
+
+# multiplication
+product = 11
+product *= 11
+print(product)
+
+product_2 = -0.00001
+product_2 *= 1000000
+print(product_2)
+
+
+# division
+divide = 50
+divide /= 7
+print(divide)
+
+divide_2 = -0.8
+divide_2 /= 45
+print(divide_2)
+
+
+# floor division
+floordiv = 47
+floordiv //= -6
+print(floordiv)
+
+floordiv_2 = 20
+floordiv_2 //= -3
+print(floordiv_2)
+
+
+# modulo
+modulus = 13
+modulus %= 7
+print(modulus)
+
+modulus_2 = -0.1
+modulus_2 %= 5
+print(modulus_2)
+
+
+# exponent
+power = 3
+power **= 3
+print(power)
+
+power_2 = 2
+power_2 **= -0.5
+print(power_2)
+
+
+# in strings
+greet = "Hello"
+greet += " World!!!"
+print(greet)
+
+yo = "Y0 "
+yo *= 5
+print(yo)
+
+
+# increment use x += 1
+num = 10
+num += 1
+print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
