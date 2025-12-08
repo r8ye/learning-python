1b255b6198704f8b4830eef0773c962f998da62f
zxraye <zxraye@gmail.com>
2025-12-08 13:38:33 +0800

    conditional statements

---
+++ Learning Python/conditional_statements.py
@@ -0,0 +1,73 @@
+print(8 == 9)
+print(1 > 2)
+print ( 67 != 67)
+print(5 >= 6)
+print(5  <= 7)
+
+
+# if statements
+age = 89
+
+if age >= 60:
+    print("ang tanda mo na")
+
+
+# if-else statements
+age = 8
+
+if age >= 18:
+    print("adult")
+else:
+    print("kid")
+
+
+score = 94
+
+if score >= 75:
+    print("you passed")
+else:
+    print("you failed")
+
+
+# elif
+marks = 88
+
+if marks >= 90:
+    print("Outstanding")
+elif marks >= 80:
+    print("Excellent")
+elif marks >= 75:
+    print("pasang awa")
+else:
+    print("u failed")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
