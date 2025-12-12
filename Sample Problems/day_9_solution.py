b268052698bca8befcf8c7a43c58dd98871404af
zxraye <zxraye@gmail.com>
2025-12-12 17:16:56 +0800

    day 9: combined day 1-8 topics

--- Sample Problems/day_9_solution.py
+++ Sample Problems/day_9_solution.py
@@ -1,18 +1,37 @@
 # problem 1
-age = int(input("Age: "))
-citizen = input("yes or no? ")
-education = input("Education? ")
-
-strip_lower_citizen = citizen.strip().lower()
-strip_lower_educ = education.strip().lower()
-
-if age >= 18 and strip_lower_citizen == "yes" and (strip_lower_educ == "bachelor" or "masters" or "phd"):
-    print("Qualified")
-else:
-    print("Not qualified")
+# age = int(input("Age: "))
+# citizen = input("yes or no? ")
+# education = input("Education? ")
+#
+# strip_lower_citizen = citizen.strip().lower()
+# strip_lower_educ = education.strip().lower()
+#
+# if age >= 18 and strip_lower_citizen == "yes" and (strip_lower_educ == "bachelor" or "masters" or "phd"):
+#     print("Qualified")
+# else:
+#     print("Not qualified")
 
 
 # problem 2
+# def math_engine(a, b):
+#     return (a + b) * (a - b) / (a // b)
+#
+# input_a = int(input("Num: "))
+# input_a += 10
+# a = input_a
+#
+# input_b = int(input("Num: "))
+# input_b *= 4
+# b = input_b
+
+#
+# result = math_engine(a, b)
+# print(result)
+
+
+# problem 3
+
+
 
 
 
