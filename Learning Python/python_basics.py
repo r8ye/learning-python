1f7a51ca8fae262629f85cb53f1f91d44742d55f
zxraye <zxraye@gmail.com>
2026-01-02 21:09:13 +0800

    python basics

---
+++ Learning Python/python_basics.py
@@ -0,0 +1,88 @@
+print("hello world")
+
+my_int = 100
+print("Integer:", my_int)
+
+my_float = 2.5
+print("Float:", my_float)
+
+my_str = "love"
+print("String:", my_str)
+
+my_bool = False
+print("Boolean:", my_bool)
+
+my_set = {1, 2, 3}
+print("Set:", my_set)
+
+my_dictionary = {"name": "Rachelle", "age": 21}
+print("Dictionary", my_dictionary)
+
+my_tuple = (5, 4, 3)
+print("Tuple:", my_tuple)
+
+my_range = range(8)
+print("Range:", my_range)
+
+my_none = None
+print("None:", my_none)
+
+my_list = [5, "cs", 4.66, True]
+print("List", my_list)
+
+
+# immutable and mutable types
+
+#type
+pangalan = "Rachelle"
+edad = 21
+
+print(type(edad))
+print(type(pangalan))
+
+# isinstance
+print(isinstance(pangalan, str))
+print(isinstance(edad, float))
+
+# Accessing Characters from Strings
+word = "computer science"
+
+print(word[0])
+print(word[8])
+print(word[11])
+
+
+# f-strings
+sentence = f"My name is {pangalan}."
+print(sentence)
+print(f"I am {edad} years old.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
