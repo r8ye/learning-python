b8c3abdc7c7d17e6b888cd0d0acafdbff795fadb
zxraye <zxraye@gmail.com>
2026-01-13 20:37:31 +0800

    range

---
+++ Learning Python/range.py
@@ -0,0 +1,20 @@
+# range in loops
+for num in range(3):
+    print(num)
+
+
+for num_2 in range (5, 10):
+    print(num_2)
+
+
+for num_3 in range(10, 100, 10):
+    print(num_3)
+
+
+for num_4 in range(88, 1, -8):
+    print(num_4)
+
+
+# range in a list
+numbers = list(range(1, 10, 3))
+print(numbers)
