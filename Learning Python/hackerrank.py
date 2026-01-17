f8dc893eeace0a89e63001a7256410250d59ac38
zxraye <zxraye@gmail.com>
2026-01-17 15:04:27 +0800

    list comprehensions

--- Learning Python/hackerrank.py
+++ Learning Python/hackerrank.py
@@ -1,5 +1,17 @@
-n = int(input("Num:" ))
-
-for i in range(n):
-    print(i ** 2)
+# n = int(input("Num:" ))
+#
+# for i in range(n):
+#     print(i ** 2)
     
+
+def swap_case(s):
+    return s.swapcase()
+
+res = swap_case("pypy 3")
+print(res)
+
+
+
+
+
+
--- Learning Python/list_comprehensions.py
+++ Learning Python/list_comprehensions.py
@@ -39,36 +39,39 @@ short_words = list(filter(is_short_word, words))
 print(short_words)
 
 
-phrases = ["hello world", "i love programming", "i hate jave", "i ate pizza", "shut up"]
+phrases = ["i love phyton", "i hate jave", "i am programming", "free code camp"]
 
 def is_long_phrase(phrase):
-    return len(phrase) > 10
+    return len(phrase) > 15
 
 long_phrase = list(filter(is_long_phrase, phrases))
 print(long_phrase)
 
 
+# map
+celsius = [15, 30, 45, 60, 75, 100]
+
+def to_fahrenheit(temperature):
+    return (temperature * 9/5) + 32
+
+fahrenheit = tuple(map(to_fahrenheit, celsius))
+print(fahrenheit)
 
 
+def calculate(n):
+    return n / 2
+
+nums = [9, 8, 7, 6, 5]
+ans = list(map(calculate, nums))
+print(ans)
 
 
+# sum
+list_numbers = [10, 15, 20, 25, 30]
+total = sum(list_numbers)
+print(total)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+another_list = [4, 8, 12, 16]
+total_another_list = sum(another_list, 6.7)
+print(total_another_list)
