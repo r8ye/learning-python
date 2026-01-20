9c959d9ccd34bc45d06d4d79ea49331370d5dc2b
zxraye <zxraye@gmail.com>
2026-01-20 21:41:18 +0800

    loops and sequences

---
+++ Learning Python/loops_and_sequences.py
@@ -0,0 +1,23 @@
+cities = ["manila", "taguig", "makati"]
+print(cities[0])
+print(cities[-1])
+
+
+dev = "Nezuko"
+print(list(dev))
+
+
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers))
+
+
+fruits = ["orange", "apple", "mango", "grapes"]
+fruits[-1] = "watermelon"
+
+print(fruits)
+
+del fruits[2]
+print(fruits)
+
+print("banana" in fruits)
+print("watermelon" in fruits)
