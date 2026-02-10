08ed882d2a23be3aeadc0ccac4e9b2bca5abc240
zxraye <zxraye@gmail.com>
2026-02-10 21:33:58 +0800

    email simulator

---
+++ Learning Python/email_simulator.py
@@ -0,0 +1,22 @@
+class Email:
+    def __init__(self, sender, receiver, subject, body):
+        self.sender = sender
+        self.receiver = receiver
+        self.subject = subject
+        self.body = body
+        self.read = False
+
+    def mark_as_read(self):
+        self.read = True
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.inbox = []
+
+    def send_email(self, receiver, subject, body):
+        email = Email(sender=self, receiver=receiver, subject=subject, body=body)
+
+class Inbox:
+    def __init__(self):
+        self.emails = []
