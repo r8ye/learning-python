c756ee7c575a1c4611de826b9eb520ad26e91ef3
zxraye <zxraye@gmail.com>
2026-02-12 23:28:19 +0800

    budget app

---
+++ Certification Projects/budget_app.py
@@ -0,0 +1,12 @@
+class Category:
+    def __init__(self, ledger):
+        self.ledger = ledger
+
+    def method(self):
+        pass
+
+    def get_balance(self):
+        pass
+
+def create_spend_chart(categories):
+    pass
